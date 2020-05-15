07ee66a7e1429b56da403cdf861579812603e235
Tim Mendoza <tmendoza@twilio.com>
2020-05-15 10:43:26 -0600

    Create list script

---
+++ serverless/scripts/list.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
+async function findApp(APP_NAME) {
+  const services = await client.serverless.services.list();
+  return services.find((service) => service.friendlyName === APP_NAME);
+}
+
+async function getAppInfo() {
+  const app = await findApp('rtc-diagnostics');
+  if (!app) return null;
+
+  const appInstance = client.serverless.services(app.sid);
+  const [environment] = await appInstance.environments.list();
+  console.log('https://' + environment.domainName);
+}
+
+getAppInfo();
