43eebdf59433d6f75f877729851502d3a2010805
Tim Mendoza <tmendoza@twilio.com>
2020-05-14 22:50:26 -0600

    Refactor some

--- serverless/scripts/deploy.js
+++ serverless/scripts/deploy.js
@@ -11,53 +11,44 @@ const serverlessClient = new TwilioServerlessApiClient({
   authToken: process.env.AUTH_TOKEN,
 });
 
-let app;
-let appInstance;
-
-function createTwiMLApp(domain) {
-  return client.applications.create({
-    voiceMethod: 'GET',
-    voiceUrl: `https://${domain}/record`,
-    friendlyName: 'Test TwiML App',
-  });
-}
-
 async function deploy() {
   const { functions } = await getListOfFunctionsAndAssets(path.join(__dirname, '..'));
-  return serverlessClient
-    .deployProject({
-      env: {
-        TWIML_APP_SID: 'foo',
-        API_KEY: process.env.API_KEY,
-        API_SECRET: process.env.API_SECRET,
-      },
-      pkgJson: {},
-      functionsEnv: 'dev',
-      assets: [],
-      functions,
-      serviceName: 'rtc-diagnostics',
-    })
-    .then((service) => {
-      app = service;
-      appInstance = client.serverless.services(app.serviceSid);
-      return app;
-    });
+  return serverlessClient.deployProject({
+    env: {
+      TWIML_APP_SID: 'foo',
+      API_KEY: process.env.API_KEY,
+      API_SECRET: process.env.API_SECRET,
+    },
+    pkgJson: {},
+    functionsEnv: 'dev',
+    assets: [],
+    functions,
+    serviceName: 'rtc-diagnostics',
+  });
 }
 
-async function updateVariable(sid) {
+async function updateVariable(app, TwiMLApp) {
+  const appInstance = await client.serverless.services(app.serviceSid);
   const environment = await appInstance.environments(app.environmentSid);
-  const variables = await environment.variables.list(); //deployment.environmentSid);
-  const TwimlAppSidVariable = variables.find(variable => variable.key === 'TWIML_APP_SID');
-  const result = await environment.variables(TwimlAppSidVariable.sid).update({ value: sid });
+  const variables = await environment.variables.list();
+  const TwimlAppSidVariable = variables.find((variable) => variable.key === 'TWIML_APP_SID');
+  return await environment.variables(TwimlAppSidVariable.sid).update({ value: TwiMLApp.sid });
+}
+
+(async () => {
+  cli.action.start('Deploying functions');
+  const app = await deploy();
+
+  cli.action.start('Registering TwiML App');
+  const TwiMLApp = await client.applications.create({
+    voiceMethod: 'GET',
+    voiceUrl: `https://${app.domain}/record`,
+    friendlyName: 'Test TwiML App',
+  });
+
+  cli.action.start('Updating configuration');
+  await updateVariable(app, TwiMLApp);
+
   cli.action.stop();
   console.log('Deployed to: https://' + app.domain);
-}
-
-cli.action.start('Deploying functions');
-deploy().then(() => {
-  cli.action.start('Registering TwiML App');
-  createTwiMLApp(app.domain).then((TwiMLApp) => {
-    cli.action.start('Updating configuration');
-    updateVariable(TwiMLApp.sid);
-  });
-});
+})();
