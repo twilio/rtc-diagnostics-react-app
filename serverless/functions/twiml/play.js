2d2be5e2a18a42e40e0fa53c39aab8d50a25c497
Tim Mendoza <tmendoza@twilio.com>
2020-05-18 12:49:56 -0600

    Let > const

--- serverless/functions/twiml/play.js
+++ serverless/functions/twiml/play.js
@@ -1,5 +1,5 @@
 exports.handler = function (context, event, callback) {
-  let twiml = new Twilio.twiml.VoiceResponse();
+  const twiml = new Twilio.twiml.VoiceResponse();
   twiml.say('You said:');
   twiml.play(event.RecordingUrl, { loop: '1' });
   twiml.say('Hanging up now.');
--- serverless/functions/twiml/record.js
+++ serverless/functions/twiml/record.js
@@ -1,5 +1,5 @@
 exports.handler = function (context, event, callback) {
-  let twiml = new Twilio.twiml.VoiceResponse();
+  const twiml = new Twilio.twiml.VoiceResponse();
   twiml.say('Record a message in 3, 2, 1');
   twiml.record({ maxLength: 3, action: `https://${context.DOMAIN_NAME}/twiml/play` });
   twiml.say('Did not detect a message to record');
