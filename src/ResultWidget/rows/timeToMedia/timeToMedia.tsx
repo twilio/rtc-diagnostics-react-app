a6cfba643113a6c39c5fce7aa93ca81b4a7332fe
Tim Mendoza <tmendoza@twilio.com>
2020-06-11 12:25:29 -0600

    Add correct content to time to media row

--- src/ResultWidget/rows/timeToMedia/timeToMedia.tsx
+++ src/ResultWidget/rows/timeToMedia/timeToMedia.tsx
@@ -13,21 +13,14 @@ const row: Row = {
   tooltipContent: {
     label: (
       <Typography>
-        Tests connectivity to the Media servers. See{' '}
-        <Link href="https://www.twilio.com/docs/voice/client/javascript/voice-client-js-and-mobile-sdks-network-connectivity-requirements#voice-media-servers-connectivity-requirements">
-          media connectivity requirements
-        </Link>{' '}
-        for more information. Unreachable media servers cause the call to drop prematurely and could cause one way audio
-        type issues.
+        The time it takes for the media to flow between the caller and callee after the callee picks up. The lower the
+        better. A high value indicates latency issues in the connection between the endpoints.
       </Typography>
     ),
     [TestWarnings.warn]: (
       <Typography>
-        The media server could not be reached. Ensure your device has internet connectivity, and any firewalls allow
-        access to Twilio’s{' '}
-        <Link href="https://www.twilio.com/docs/voice/client/javascript/voice-client-js-and-mobile-sdks-network-connectivity-requirements#voice-media-servers-connectivity-requirements">
-          Media servers
-        </Link>
+        High time to media results in a bad user experience when the call is first picked up. Run the test a few times
+        to ensure this is not a glitch in the network.
       </Typography>
     ),
   },
