3512d3d82920b4f65d77d22f9034beadc28a6fa6
Tim Mendoza <tmendoza@twilio.com>
2020-06-12 12:19:08 -0600

    Update tests

--- serverless/__tests__/functions/app/turn-credentials.ts
+++ serverless/__tests__/functions/app/turn-credentials.ts
@@ -10,7 +10,7 @@ describe('the turn-credentials function', () => {
   it('should return turn credentials', () => {
     const mockCallback = jest.fn();
     handler({ getTwilioClient: () => mockClient }, {}, mockCallback);
-    expect(mockClient.tokens.create).toHaveBeenCalledWith({ ttl: 60 });
+    expect(mockClient.tokens.create).toHaveBeenCalledWith({ ttl: 180 });
     setImmediate(() => {
       expect(mockCallback).toHaveBeenCalledWith(null, 'mock token');
     });
