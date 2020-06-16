2a20b02984e1e9824e6fd61bb2b640a72785c669
Tim Mendoza <tmendoza@twilio.com>
2020-06-16 14:19:47 -0600

    Update snapshots

--- serverless/__tests__/functions/twiml/play.ts
+++ serverless/__tests__/functions/twiml/play.ts
@@ -5,7 +5,7 @@ describe('the "play" TwiML function', () => {
     const mockCallback = jest.fn();
     handler({}, { RecordingUrl: 'testurl.com/recording' }, mockCallback);
     expect(mockCallback.mock.calls[0][1].toString()).toMatchInlineSnapshot(
-      `"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><Response><Say>You said:</Say><Play loop=\\"1\\">testurl.com/recording</Play><Say>Hanging up now.</Say></Response>"`
+      `"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><Response><Say>You said:</Say><Play loop=\\"1\\">testurl.com/recording</Play><Say>Now waiting for a few seconds to gather audio performance metrics.</Say><Pause length=\\"3\\"/><Say>Hanging up now.</Say></Response>"`
     );
   });
 });
