4d74afa85b447841d15011b36fe3779e671d8234
timmydoza <tmendoza@twilio.com>
2020-06-30 10:11:22 -0600

    Remove spaces from default identity (#11)

--- serverless/constants.js
+++ serverless/constants.js
@@ -4,5 +4,5 @@ module.exports = {
   SERVICE_NAME: 'rtc-diagnostics',
   API_KEY_NAME: 'RTC Diagnostics Key',
   TWIML_APP_NAME: 'Test TwiML App',
-  VOICE_IDENTITY: process.env.VOICE_IDENTITY || 'RTC Diagnostics Test Identity',
+  VOICE_IDENTITY: process.env.VOICE_IDENTITY || 'RTC_Diagnostics_Test_Identity',
 };
