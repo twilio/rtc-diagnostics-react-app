d297aadb5b015c68c7690912a61d41cd46b23b9c
Tim Mendoza <tmendoza@twilio.com>
2020-07-02 14:25:34 -0600

    Move useTestRunner file to its own directory

--- src/NetworkTestWidget/NetworkTestWidget.tsx
+++ src/NetworkTestWidget/NetworkTestWidget.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Typography } from '@material-ui/core';
-import useTestRunner from './useTestRunner';
+import useTestRunner from './useTestRunner/useTestRunner';
 import RegionResult from './RegionResult/RegionResult';
 import { Region } from '../types';
 
