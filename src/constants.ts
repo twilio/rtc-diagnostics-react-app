72884ce4003721f25fdbb7a2d66e5f3774d685fa
Tim Mendoza <tmendoza@twilio.com>
2020-07-28 15:31:47 -0600

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import { Region } from './types';
+import { Connection } from 'twilio-client';
+
+export const DEFAULT_REGIONS: Region[] = ['roaming', 'dublin'];
+
+export const DEFAULT_CODEC_PREFERENCES: Connection.Codec[] = [Connection.Codec.Opus, Connection.Codec.PCMU];
