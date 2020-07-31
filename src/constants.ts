845a82c448aa8eccd0b2b4b23d28214b3bd42fea
timmydoza <tmendoza@twilio.com>
2020-07-31 14:42:18 -0600

    Update src/constants.ts to match SDK defaults
    Co-authored-by: Charlemagne Santos <csantos@twilio.com>

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import { Connection } from 'twilio-client';
 
 export const DEFAULT_REGIONS: Region[] = ['roaming', 'dublin'];
 
-export const DEFAULT_CODEC_PREFERENCES: Connection.Codec[] = [Connection.Codec.Opus, Connection.Codec.PCMU];
+export const DEFAULT_CODEC_PREFERENCES: Connection.Codec[] = [Connection.Codec.PCMU, Connection.Codec.Opus];
