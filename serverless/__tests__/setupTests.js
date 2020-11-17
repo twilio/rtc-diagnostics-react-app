2e00372b3ed33f2a870390cc07db33e0a8e650f7
Tim Mendoza <tmendoza@twilio.com>
2020-11-16 22:42:38 -0700

    Add tests for verify_expiry asset

--- serverless/__tests__/setupTests.js
+++ serverless/__tests__/setupTests.js
@@ -1,4 +1,25 @@
+class Response {
+  constructor() {
+    this.statusCode = null;
+    this.body = null;
+    this.headers = {};
+  }
+
+  setStatusCode(code) {
+    this.statusCode = code;
+  }
+
+  setBody(body) {
+    this.body = body;
+  }
+
+  appendHeader(key, value) {
+    this.headers[key] = value;
+  }
+}
+
 global.Twilio = require('twilio');
+global.Twilio.Response = Response;
 
 process.on('unhandledRejection', (err) => {
   throw err;
