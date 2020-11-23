1d0043e6aaecd891d76164e431762dcf6827d36f
Tim Mendoza <tmendoza@twilio.com>
2020-11-22 17:31:44 -0700

    Display errors in tooltipContent

--- src/NetworkTestWidget/EdgeResult/getTooltipContent.tsx
+++ src/NetworkTestWidget/EdgeResult/getTooltipContent.tsx
@@ -4,6 +4,12 @@ import { TestResults } from '../../types';
 import { Typography as TooltipTypography } from '../../ResultWidget/rows/shared';
 
 export default function getTooltipContent(result: TestResults): React.ReactElement[] {
+  if (result.errors.bitrate?.message === 'expired' || result.errors.preflight?.message === 'expired') {
+    return [
+      <TooltipTypography key="expired">App has expired. Please redeploy the app and try again.</TooltipTypography>,
+    ];
+  }
+
   const warnings = rows
     .map((row) => {
       const warning = row.getWarning?.(result);
