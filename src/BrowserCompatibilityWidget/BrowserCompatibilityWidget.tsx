31a06186108cb0a25432a974eb771ad9c2186f63
timmydoza <tmendoza@twilio.com>
2020-11-30 11:16:40 -0700

    Ahoyapps 648 browser compatibility (#35)
    * Install nanoid and moment
    
    * Update serverless scripts.
    Add random characters to service name and add environment variable for app expiry
    
    * Add verify_expiry asset and add it to other functions
    
    * Add tests for verify_expiry asset
    
    * Mock verify_expiry function so that all other tests continue to pass
    
    * Update e2e tests
    
    * Change fetch functions so that they can throw errors from the API
    
    * Create Alert Component
    
    * Refactor SummaryWidget to use Alert
    
    * Show Alert in NetworkTestWidget when app is expired
    
    * Move Alert component to common folder
    
    * Adjust spacing in Alert component
    
    * Add tests for Alert component
    
    * Fix SummaryWidget tests
    
    * Fix NetworkTestWidget tests
    
    * Display errors in tooltipContent
    
    * Add tests for getTooltipContent and NetworkTestWidget
    
    * Add BrowserCompatibilityWidget and tests
    
    * Update styles of Alert component
    
    * Update Alert styles and snapshot tests
    
    * Change Alert variant 'info' to 'success' and update styles
    
    * Fix SummaryWidget tests
    
    * Remove moment
    
    * Change 'assets' directory to 'middleware'
    
    * Update App.tsx so it doesn't render the NetworkTestWidget when the browser is not supported

---
+++ src/BrowserCompatibilityWidget/BrowserCompatibilityWidget.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Alert from '../common/Alert/Alert';
+import { Link as LinkImpl, Typography } from '@material-ui/core';
+import { styled } from '@material-ui/core';
+
+const Link = styled(LinkImpl)({
+  fontWeight: 600,
+  textDecoration: 'underline',
+});
+
+export default function () {
+  return (
+    <Alert variant="error">
+      <Typography variant="body1">
+        Browser not suported. Please open this application in one of the{' '}
+        <Link
+          href="https://www.twilio.com/docs/voice/client/javascript#supported-browsers"
+          target="_blank"
+          rel="noopener"
+        >
+          supported browsers
+        </Link>
+        .
+      </Typography>
+      <Typography variant="body1">
+        If you are using a supported browser, please ensure that this app is served over a{' '}
+        <Link
+          href="https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts"
+          target="_blank"
+          rel="noopener"
+        >
+          secure context
+        </Link>
+        .
+      </Typography>
+    </Alert>
+  );
+}
